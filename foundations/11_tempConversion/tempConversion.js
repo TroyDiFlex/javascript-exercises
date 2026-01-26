2599637c76266ca2fc3c15d6627428307cb38225
Artem Sokur <teebdol@bk.ru>
2026-01-27 01:11:13 +0400

    Сделал одиннадцатое упражнение основ

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  let cels = (fahr - 32) * (5 / 9);
+  return Number(cels.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cels) {
+  let fahr = cels * (9 / 5) + 32;
+  return Number(fahr.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
