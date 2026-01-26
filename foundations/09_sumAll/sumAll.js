bdc5a1a922807482711f315d4810f15c6cc9e4fc
Artem Sokur <teebdol@bk.ru>
2026-01-27 00:02:20 +0400

    Сделал девятое упражнение основ

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let count = 0;
+  if (
+    a < 0 ||
+    b < 0 ||
+    a % 1 !== 0 ||
+    b % 1 !== 0 ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  )
+    return "ERROR";
+  if (a < b) {
+    for (a; a <= b; a++) count += a;
+    return count;
+  } else {
+    for (b; b <= a; b++) count += b;
+    return count;
+  }
 };
 
 // Do not edit below this line
