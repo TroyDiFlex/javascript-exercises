52d18ea15514647fa33a45154834d88fc566dc57
Artem Sokur <teebdol@bk.ru>
2026-01-26 23:35:08 +0400

    Сделал восьмое упражнение основ

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let currentIndex = 0;
+  for (let j = arr.length; j > 0; j--) {
+    for (let i = args.length - 1; i >= 0; i--) {
+      currentIndex = arr.indexOf(args[i]);
+      if (currentIndex < 0) continue;
+      arr.splice(currentIndex, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
