61fc58d68630b731e4282990ecf688a1367c1aa8
Artem Sokur <teebdol@bk.ru>
2026-02-13 06:09:06 +0400

    Сделал 13-е упражнение основ

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const cleanString = word.toLowerCase().replace(/[^\p{L}\p{N}]/gu, "");
+  for (let i = 0; i < Math.floor(cleanString.length / 2); i++) {
+    if (cleanString[i] !== cleanString.at(-i - 1)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
