d075522a48fb0e0a9bf5c60fb522c67dbfce4665
Artem Sokur <teebdol@bk.ru>
2026-02-14 05:29:29 +0400

    Решил 14-е упражнение основ

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (+num < 0) return "OOPS";
+  if (+num === 0) return 0;
+  let previous = 0;
+  let current = 1;
+  let clipboard;
+  for (let i = 1; i < +num; i++) {
+    clipboard = current;
+    current += previous;
+    previous = clipboard;
+  }
 
+  return current;
 };
 
 // Do not edit below this line
