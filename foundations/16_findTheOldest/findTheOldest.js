714669b3d39ff1c25a29bf281f1e4c19144d10e4
Artem Sokur <teebdol@bk.ru>
2026-02-14 23:04:19 +0400

    Решил 16-е упражнение основ

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const getCurrentYear = () => new Date().getFullYear();
+  const getAge = (yearOfBirth, yearOfDeath) =>
+    (yearOfDeath ?? getCurrentYear()) - yearOfBirth;
 
+  const theOldestMan = arr.sort((a, b) => {
+    const aAge = getAge(a.yearOfBirth, a.yearOfDeath);
+    const bAge = getAge(b.yearOfBirth, b.yearOfDeath);
+    return bAge - aAge;
+  })[0];
+  return theOldestMan;
 };
 
 // Do not edit below this line
